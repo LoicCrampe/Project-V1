43f293d2677012def3d9739a004760d28494c8aa
Loïc Bureau <loicf.crampe@gmail.com>
2021-05-30 00:06:19 +0200

    Ajout de Font Awesome et modification du Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
 
+// Font Awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 function Header() {
     return <div>
-        Header
+        <div>
+            <img src="./public/Logo.png"/>
+            <div>
+                <FontAwesomeIcon icon={faSearch} />
+                <input type="text" name="search" />
+            </div>
+        </div>
     </div>
 }
 
